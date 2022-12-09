393c5dd6503ae0d282c0c060f426888497b0c430
Julio Isaac David Ramos <isaacdvid15.96@gmail.com>
2022-12-08 19:59:50 -0600

    Add files via upload

---
+++ DotNetWeb.Core/ArithmethicExpression.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DotNetWeb.Core
+{
+    public class ArithmethicExpression : BinaryExpression
+    {
+        private readonly Dictionary<(ExpressionType, ExpressionType, TokenType), ExpressionType> _typeRules;
+        public Token Operation { get; }
+
+        public ArithmeticExpression(Expression leftExpression, Expression rightExpression, Token operation)
+            : base(leftExpression, rightExpression)
+        {
+            Operation = operation;
+            _typeRules = new Dictionary<(ExpressionType, ExpressionType, TokenType), ExpressionType>
+        {
+            {(ExpressionType.Int, ExpressionType.Int, TokenType.Plus), ExpressionType.Int},
+            {(ExpressionType.Int, ExpressionType.Int, TokenType.Minus), ExpressionType.Int},
+            {(ExpressionType.Int, ExpressionType.Int, TokenType.Asterisk), ExpressionType.Int},
+            {(ExpressionType.Int, ExpressionType.Int, TokenType.Division), ExpressionType.Int},
+
+            {(ExpressionType.Float, ExpressionType.Float, TokenType.Plus), ExpressionType.Float},
+            {(ExpressionType.Float, ExpressionType.Float, TokenType.Minus), ExpressionType.Float},
+            {(ExpressionType.Float, ExpressionType.Float, TokenType.Asterisk), ExpressionType.Float},
+            {(ExpressionType.Float, ExpressionType.Float, TokenType.Division), ExpressionType.Float},
+
+            {(ExpressionType.Int, ExpressionType.Float, TokenType.Plus), ExpressionType.Float},
+            {(ExpressionType.Float, ExpressionType.Int, TokenType.Plus), ExpressionType.Float},
+
+            {(ExpressionType.String, ExpressionType.String, TokenType.Plus), ExpressionType.String},
+        };
+        }
+    }
+}
---
+++ DotNetWeb.Core/BinaryExpression.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DotNetWeb.Core
+{
+    public class BinaryExpression : Expression
+    {
+        public Expression LeftExpression { get; set; }
+        public Expression RightExpression { get; set; }
+
+        public BinaryExpression(Expression leftExpression, Expression rightExpression)
+        {
+            LeftExpression = leftExpression;
+            RightExpression = rightExpression;
+        }
+    }
+}
